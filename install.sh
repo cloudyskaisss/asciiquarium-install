289371dc23d5e98d4a29ca78edde335b4b129155
cloudyskaisss <146793021+cloudyskaisss@users.noreply.github.com>
2023-10-30 11:37:13 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,19 @@
-# install perl::animation dependency
-sudo apt install libcurses-perl
-wget https://cpan.metacpan.org/authors/id/K/KB/KBAUCOM/Term-Animation-2.5.tar.gz
-tar -xzf Term-Animation-2.5.tar.gz
-perl Term-Animation-2.5/Makefile.PL
-make
+# set variables
+$OD = $(pwd)
+
+# install dependencies
+sudo apt install libcurses-perl make
+sudo wget https://cpan.metacpan.org/authors/id/K/KB/KBAUCOM/Term-Animation-2.5.tar.gz
+sudo tar -zxf Term-Animation-2.5.tar.gz
+cd Term-Animation-2.5
+sudo perl Makefile.PL
+sudo make
 sudo make install
 
 # install asciiquarium as a command
-sudo wget http://www.robobunny.com/projects/asciiquarium/asciiquarium.tar.gz -O /tmp/asciiquarium.tar.gz
-sudo tar -zxf /tmp/asciiquarium.tar.gz
-sudo cp asciiquarium_1.1/asciiquarium /usr/local/bin
+cd /tmp
+sudo wget http://www.robobunny.com/projects/asciiquarium/asciiquarium.tar.gz
+sudo tar -zxf asciiquarium.tar.gz
+cd asciiquarium_1.1
+sudo cp asciiquarium /usr/local/bin
 sudo chmod 0755 /usr/local/bin/asciiquarium
